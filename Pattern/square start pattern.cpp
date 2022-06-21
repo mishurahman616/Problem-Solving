c15d11ddeee5435c03d8708ef7154ac375faa457
Mishu Rahman <mishu.cse616@gmail.com>
2022-06-22 00:23:32 +0600

    patter added

---
+++ Pattern/square start pattern.cpp
@@ -0,0 +1,21 @@
+/*
+if length of one side n is given then number of star can be found
+    *****
+    *****
+    *****
+    *****
+    *****
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
