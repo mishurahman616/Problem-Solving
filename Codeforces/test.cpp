0cdec4e3dcedccc091d43e6d6b3c4231532c6c21
Mishu Rahman <mishu.cse616@gmail.com>
2022-10-26 01:10:03 +0600

    Test file changed

--- Codeforces/test.cpp
+++ Codeforces/test.cpp
@@ -1,17 +1,13 @@
 #include <bits/stdc++.h>
-#define kiInputNibo cin>>
-#define kiPrintkorbo cout<<
+#define inputNeo cin>>
+#define printKoro cout<<
+#define lineSesh <<endl;
 #define obossoiLagbe main()
 using namespace std;
 
 int obossoiLagbe
 {
-    int mishu;
-    string str;
-    kiInputNibo str;
-    mishu = stoi(str);
-
-    kiPrintkorbo mishu;
-
-
+int x[1] ={6};
+printKoro (x[0]>>1) lineSesh
+printKoro (x[0]>>1) lineSesh
 }
