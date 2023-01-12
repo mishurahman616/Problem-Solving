ccd4cb0eec6a37cf0ce04ae8c840a4d142f611af
Mishu Rahman <mishu.cse616@gmail.com>
2023-01-12 15:12:46 +0600

    SuperDigit

---
+++ HackerRank/SuperDigit/SuperDigit/Program.cs
@@ -0,0 +1,13 @@
+﻿int superDigit(string n, int k)
+{
+    long sum = 0;
+    foreach (char c in n)
+    {
+        sum += c - '0';
+    }
+    sum *= k;
+    if (sum % 10 == sum) return Convert.ToInt32(sum);
+    else return superDigit(sum.ToString(), 1);
+}
+
+Console.WriteLine(superDigit("2345566345634563456546", 2));
