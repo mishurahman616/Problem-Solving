0602b6dd40a254d987199a8dd60266594993c3bc
Mishu Rahman <mishu.cse616@gmail.com>
2023-01-12 14:36:30 +0600

    Sherlock and Array

---
+++ HackerRank/SherlockAndArray/SherlockAndArray/Program.cs
@@ -0,0 +1,32 @@
+﻿ string balancedSums(List<int> arr)
+{
+    int i = 0, j = arr.Count - 1;
+    long leftSum = 0, rightSum = 0;
+
+    while (i != j)
+    {
+        if (leftSum + arr[i] <= rightSum + arr[j])
+        {
+            leftSum += arr[i];
+            i++;
+        }
+        else if (rightSum + arr[j] <= leftSum + arr[i])
+        {
+            rightSum += arr[j];
+            j--;
+        }
+    }
+
+
+
+    if (leftSum == rightSum)
+    {
+        return "YES";
+    }
+    else
+    {
+        return "NO";
+    }
+
+}
+Console.WriteLine(balancedSums(new List<int> {1, 2, 3 }));
