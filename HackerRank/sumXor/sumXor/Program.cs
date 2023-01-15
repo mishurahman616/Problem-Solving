d6079165a951b7d537d7857497358b0516a416e0
Mishu Rahman <mishu.cse616@gmail.com>
2023-01-15 18:08:59 +0600

    SumXor and Other Test code

---
+++ HackerRank/sumXor/sumXor/Program.cs
@@ -0,0 +1,58 @@
+﻿using sumXor;
+
+//long n = 800;
+//long counter = 0;
+//long x = 0;
+//while (x <= n)
+//{
+//    Console.WriteLine($"{n|x}    {n^x}    {x}");
+//    if ((n | x) == (n ^ x)) counter++;
+//    x++;
+//}
+//List<int> asd = new List<int>() { 4, 3, 5 };
+//asd.Sort();
+//Console.WriteLine(asd[0]);
+//string s = Convert.ToString(n, 2);
+//int z = s.Count(ch => ch == '0');
+//Console.WriteLine(Math.Pow(2, z));
+//Console.WriteLine(z);
+//Console.WriteLine(x);
+//Console.WriteLine(counter);
+
+
+//others
+//List<int> a = new List<int>() { 2,4 };
+//List<int> b = new List<int>() { 16,32,96 };
+
+//int counter = 0;
+//int lcm = Result.findLCM(a);
+//int gcf = Result.findGCF(b);
+//for (int i = lcm; i <= gcf; i += lcm)
+//{
+//    if (gcf % i == 0)
+//    {
+//        counter++;
+//    }
+//}
+//Console.WriteLine(counter); 
+
+string s = "xaxbbbxx";
+char[] s2 = s.Substring(0, s.Length / 2).ToCharArray() ; 
+Array.Sort(s2) ;
+char[] s1 = s.Substring(s.Length/2, s.Length/2).ToCharArray();
+Array.Sort(s1);
+int counter = 0;
+bool[] visited = new bool[s.Length/2];
+for (int i = 0; i < s.Length / 2; i++)
+{
+    for (int j = 0; j < s.Length / 2; j++)
+    {
+        if (!(visited[j]) && s1[i] == s2[j])
+        {
+            counter++;
+            visited[j] = true;
+        }
+    }
+
+}
+Console.WriteLine(s.Length/2-counter);
---
+++ HackerRank/sumXor/sumXor/Result.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumXor
+{
+    public class Result
+    {
+        /*
+     * Complete the 'getTotalX' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+        public static int CalculateLCM(int a, int b)
+        {
+            return (b / CalculateGCF(a, b)) * a;
+        }
+        public static int findLCM(List<int> a)
+        {
+            a.Sort();
+            int lcm = a[0];
+            for (int i = 1; i < a.Count; i++)
+            {
+                lcm = Result.CalculateLCM(lcm, a[i]);
+            }
+            return lcm;
+        }
+        public static int CalculateGCF(int a, int b)
+        {
+            if (b == 0) return a;
+            return CalculateGCF(b, a % b);
+        }
+        public static int findGCF(List<int> b)
+        {
+            b.Sort();
+            int gcf = b[0];
+            for (int i = 1; i < b.Count; i++)
+            {
+                gcf = Result.CalculateGCF(b[i], gcf);
+            }
+            return gcf;
+        }
+    }
+}
